32783487d7769a157610c6236f3098011174fac6
Prerna Dhareshwar <prernamd@stanford.edu>
2019-05-14 10:40:06 -0700

    Training on Gcloud version

--- model.py
+++ model.py
@@ -43,38 +43,40 @@ Generator = nn.Sequential(
 )
 
 Discriminator = nn.Sequential(
-    nn.Conv3d(3, 32, 3, stride = 1, padding = 1),
-    nn.MaxPool3d(3),
+    nn.Conv3d(3, 8, 3, stride = 1, padding = 1),
+    nn.MaxPool3d(2),
+    nn.BatchNorm3d(8),
+    nn.LeakyReLU(0.2, True),
+    nn.Conv3d(8, 16, 3, stride = 1, padding = 1),
+    nn.MaxPool3d(2),
+    nn.BatchNorm3d(16),
+    nn.LeakyReLU(0.2, True),
+    nn.Conv3d(16, 32, 3, stride = 1, padding = 1),
+    nn.MaxPool3d(2),
     nn.BatchNorm3d(32),
     nn.LeakyReLU(0.2, True),
     nn.Conv3d(32, 64, 3, stride = 1, padding = 1),
-    nn.MaxPool3d(3),
+    nn.MaxPool3d(2),
     nn.BatchNorm3d(64),
-    nn.LeakyReLU(0.2, True),
-    nn.Conv3d(64, 128, 3, stride = 1, padding = 1),
-    nn.MaxPool3d(3),
-    nn.BatchNorm3d(128),
     nn.LeakyReLU(0.2, True),
     nn.MaxPool3d(2),
     View_fun(),
-    nn.Linear(128, 1),
+    nn.Linear(256, 1),
     nn.Sigmoid(),
 )
-
 cuda = torch.cuda.is_available()
 device = torch.device('cuda:0' if cuda else 'cpu')
 
-generator = generator.to(device)
-discriminator = discriminator.to(device)
-
 X = {}
-loc = "Users/prerna/Documents/Q3/CS231n/data/cartwheel"
+loc = "/home/prerna/data"
 f_list = []
 for filename in os.listdir(loc):
     f_list.append(loc + '/' + filename)
 
 lr_val = 0.00005
-num_epochs = 1
+num_epochs = 50
+Generator = Generator.cuda()
+Discriminator = Discriminator.cuda()
 optim_G = torch.optim.RMSprop(Generator.parameters(), lr = lr_val)
 optim_D = torch.optim.RMSprop(Discriminator.parameters(), lr = lr_val)
 
@@ -84,6 +86,11 @@ loss_dis = []
 ctr = 0
 t = 0
 for n in range(num_epochs):
+    if float(n+1)%50 == 0:
+        path_parameter_G = "output/" + "inter_G_" +str(lr_val)+ '_'+ str(n+1) + ".pt"
+        path_parameter_D = "output/" + "inter_D_" +str(lr_val)+ '_'+ str(n+1) + ".pt"
+        torch.save(Generator.state_dict(), path_parameter_G)
+        torch.save(Discriminator.state_dict(), path_parameter_D)
     for f in f_list:
         ctr += 1
         print(ctr)
@@ -104,15 +111,23 @@ for n in range(num_epochs):
             Y_gen = Y_gen.permute(0, 2, 3, 4, 1)
             Y_dis = Discriminator(Y_gen)
             
-            optim_D.zero_grad()
-            loss_D = -torch.mean(torch.log(Discriminator(X_real))) - torch.mean(torch.log(1 - Y_dis))         
-            loss_D.backward(retain_graph = True)
-            optim_D.step()
-            
             optim_G.zero_grad()
             loss_G = -torch.mean(torch.log(Y_dis))
             loss_G.backward()
             optim_G.step()
+            
+            X = X_full[idx*32:(idx+1)*32]
+            X_real = X.view(1, X.shape[0], X.shape[1], X.shape[2], X.shape[3])
+            X_real = X_real.permute(0, 2, 3, 4, 1)
+            Y_gen = Generator(X)
+            Y_gen = Y_gen.view(1, Y_gen.shape[0], Y_gen.shape[1], Y_gen.shape[2], Y_gen.shape[3])
+            Y_gen = Y_gen.permute(0, 2, 3, 4, 1)
+            Y_dis = Discriminator(Y_gen)
+            
+            optim_D.zero_grad()
+            loss_D = -torch.mean(torch.log(Discriminator(X_real))) - torch.mean(torch.log(1 - Y_dis))         
+            loss_D.backward()
+            optim_D.step()
             
             loss_gen.append(loss_G)
             loss_dis.append(loss_D)
